925ecbcb187807c3d76da493ddf27c551091d1f6
Vinicius Duarte Passo <92999708+ViniciusDuarte17@users.noreply.github.com>
2023-11-09 10:31:03 -0300

    vaiaveis em java

--- IntroducaoAoJava/variaveis/Main.java
+++ IntroducaoAoJava/variaveis/Main.java
@@ -3,6 +3,24 @@ package IntroducaoAoJava.variaveis;
 public class Main {
    
     public static void main (String [] args) {
-        System.out.println("Hello world");
+     String nome = "vinicius";
+
+     System.out.println("Olá, " + nome);
+
+     int a;
+     int b = 2;
+
+     a = 3;
+
+     int soma = a + b;
+     int subtracao = a - b;
+     int mutiplicacao = a * b;
+     float divisao = (float) a / b;   
+
+     System.out.println(soma);
+     System.out.println(subtracao);
+     System.out.println(mutiplicacao);
+     System.out.println(divisao);
+     
     }
 }
